367a8ebb82072e957e895e172ec3fc47720aed36
Emerson Gonzalez <emersonjavid97@gmail.com>
2020-05-05 16:52:49 -0500

    add timer label, play sound, diccionary

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,31 +7,66 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
-    let sofTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    var doneSoundEffect: AVAudioPlayer?
+    let eggTime = ["Soft" : 3, "Meddium": 4, "Hard": 7]
+    var titleButtom: String = ""
+    var timer: Timer?
+    var timeLeft = 0
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var LabelInstructions: UILabel!
+    @IBOutlet weak var LabelTime: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        progressView.progress = 0.0
+        LabelTime.text = "Time"
     }
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
-        
-        switch sender.currentTitle {
-        case "Soft":
-            print(sofTime)
-        case "Meddium":
-            print(mediumTime)
-        case "Hard":
-            print(hardTime)
-        default:
-            print("sound no exits")
+        titleButtom = sender.currentTitle!
+        LabelInstructions.text = titleButtom
+        timeLeft = eggTime[titleButtom]!
+        LabelTime.text = "\(Int(timeLeft) / 60 % 60):\(Int(timeLeft) % 60)"
+        startTimer()
+    }
+ 
+    func startTimer() {
+        progressView.progress = 1.0
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            self.timeLeft -= 1
+            self.progressView.progress = Float(self.timeLeft)/Float(self.eggTime[self.titleButtom]!)
+            
+                let minutesLeft = Int(self.timeLeft) / 60 % 60
+                let secondsLeft = Int(self.timeLeft) % 60
+                self.LabelTime.text = "\(minutesLeft):\(secondsLeft)"
+            if self.timeLeft <= 0 {
+                self.finishTimer()
+                self.LabelInstructions.text = "DONE!"
+                self.playSound(soundName: "alarm_sound")
+            }
         }
     }
     
+    func finishTimer() {
+        timer?.invalidate()
+    }
+    
+    func playSound(soundName: String) {
+        let path = Bundle.main.path(forResource: soundName, ofType: "mp3")!
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            doneSoundEffect = try AVAudioPlayer(contentsOf: url)
+            doneSoundEffect?.play()
+        } catch {
+            print("error play the sound:: \(error)")
+        }
+    }
 }
 
