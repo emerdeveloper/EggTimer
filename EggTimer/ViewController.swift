755e0f35a1e3540a5985389c7fed03eb7e21391e
Emerson Gonzalez <emersonjavid97@gmail.com>
2020-05-05 12:19:02 -0500

    Initial Commit

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Emerson Javid Gonzalez Morales on 5/05/20.
+//  Copyright © 2020 Emerson Javid Gonzalez Morales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
